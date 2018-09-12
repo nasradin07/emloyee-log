1b7c7adc80a28a07d711b37ee31ece9784855321
Radovan Stevanovic <rasha08@gmail.com>
2018-09-12 20:21:19 +0200

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Text colors.
+RED_COLOR='\033[0;31m';
+GREEN_COLOR='\033[0;32m';
+NO_COLOR='\033[0m';
+user="root"
+host="174.138.10.209";
+
+
+logToTerminal () {
+  local message=$1;
+  local delay=$2;
+  local newLineBefore=$3;
+
+  logNewlineAndResetColor $newLineBefore
+  echo $message;
+  logNewlineAndResetColor $newLineBefore
+
+  if [ ! -z "$delay" ]
+  then
+    sleep $delay;
+  fi
+}
+
+logNewlineAndResetColor () {
+  if [ ! -z "$1" ]
+  then
+    echo "${NO_COLOR}";
+  fi
+}
+
+buildApplication () {
+  logToTerminal "${GREEN_COLOR}E sad build-am aplikaciju" 1 "yes";
+
+  npm run build --prod
+}
+
+copyFiles () {
+  sshUser=$1;
+  sshHost=$2;
+
+  logToTerminal "${GREEN_COLOR}Kopiram fajlove" 1 "yes";
+
+  scp -r ./dist/* "${sshUser}@${sshHost}:/var/www/scs-employee-log.ml/html";
+}
+
+logHello () {
+  logToTerminal "${GREEN_COLOR}Pozdra" 1;
+  logToTerminal "${RED_COLOR}v" 2;
+}
+
+logFinish () {
+  logToTerminal "${GREEN_COLOR}SVE SAM URADIO" 1 "yes"
+  logHello
+}
+
+logHello
+
+buildApplication
+
+copyFiles $user $host
+
+logFinish $1
